89d3028c7050fc0f387c9d60ac4ba7efb1371a7f
ixnp <enprocopios@gmail.com>
2023-02-24 09:50:37 -0800

    updated validations

--- 04_client_server_communication_II/flatiron_theater_company/app/models/production.rb
+++ 04_client_server_communication_II/flatiron_theater_company/app/models/production.rb
@@ -1,4 +1,18 @@
 class Production < ApplicationRecord
-    validates :title, presence: true
-    validates :budget, numericality: {greater_than: 0}
+    validates_presence_of :title, :budget, :director, :description, :image, :genre
+    validates :title, length: {maximum:50}
+    validates :description, length:{in: 5...250}
+    validates :budget, numericality: {greater_than:0, message:"$$$$More Money Plzzz$$$$$$"}
+    validates :image, format: {with:%r{.(jpg|png)\Z}i, message:"The file type must be a jpg or png"  }
+    
+    validate :genre, :no_more_musicals
+
+    def no_more_musicals
+        
+        # If we are making a Musical
+        # And if there are Musicals that are currently ongoing 
+        if self.genre == "Musical" && Production.where(genre:'Musical',ongoing:true).count > 0
+            errors.add :genre, "Sorry no more Musicals"
+        end 
+    end 
 end
